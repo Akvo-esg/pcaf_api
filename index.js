01bfd7ae5814dd1374f84bd9def7cfc2db9e53ce
JOAO SERAFIN <admin@akvo-esg.com.br>
2023-05-19 08:36:59 -0300

    Feat: teste calculo

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.json({message: "foi caraio"})
+    return res.json({ message: "Server is up" })
+})
+
+app.get('/atualizou', (req, res) => {
+    return res.json({ message: 'atualizou is up' })
+})
+
+app.post('/teste', (req, res) => {
+    const { c1, c2, c3 } = req.body
+
+    const result = c1 + c2 + c3
+
+    return res.json({ result: result })
 })
 
 app.listen(3333)
