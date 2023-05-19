06620b6e64356252964f1546ed0131266094fabb
JOAO SERAFIN <admin@akvo-esg.com.br>
2023-05-19 14:34:26 -0300

    teste console.log

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/atualizou', (req, res) => {
     return res.json({ message: 'atualizou is up' })
 })
 
+app.get('/meupau', (req, res) => {
+    return res.json({ message: 'atualizou is up' })
+})
+
 app.post('/teste', (req, res) => {
     const { c1, c2, c3 } = req.body
 
